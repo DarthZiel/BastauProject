59e02f035d0ad969ca2b09a1a48bf4c9ce05aaf7
Darth Ziel <Studerzhan@gmail.com>
2022-02-05 00:04:49 +0600

    'init'

---
+++ BastauApp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+class UserAdmin():
+    pass
+
+class CaseAdmin():
+    pass
+
+class AnswerAdmin():
+    pass
---
+++ BastauApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    pass
+
+
+class Case(models.Model):
+    pass
+
+class Answer(models.Model):
+    pass
+
+    
