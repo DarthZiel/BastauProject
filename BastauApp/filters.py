7fd07917b45869603b4d277d65bdac7adc471505
MarquisLim <mat.arman1221@gmail.com>
2022-05-01 14:04:09 +0600

    search by tags

--- BastauApp/filters.py
+++ BastauApp/filters.py
@@ -4,6 +4,7 @@ from .models import *
 
 class CaseFilter(django_filters.FilterSet):
     title = CharFilter(field_name='title', lookup_expr='icontains')
+    tags = CharFilter(field_name='tags__name', lookup_expr='icontains')
     class Meta:
         model = Case
         fields = ['category', 'region']
