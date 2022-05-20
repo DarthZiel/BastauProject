b8b7e49607a3b1a22a36ef92e49c9571aa529d5a
MarquisLim <mat.arman1221@gmail.com>
2022-05-20 19:11:55 +0600

    obnova brawl stars

--- BastauApp/filters.py
+++ BastauApp/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from django_filters import DateFilter, CharFilter, FilterSet
+from django_filters import DateFilter, CharFilter, FilterSet, ChoiceFilter
 from django.forms import ModelForm, TextInput, Textarea, Select, DateTimeInput, EmailField, CheckboxInput
 from .models import *
 
@@ -10,7 +10,8 @@ class CaseFilter(django_filters.FilterSet):
     tags = CharFilter(field_name='tags__name', lookup_expr='icontains', widget = TextInput(attrs={
             'class':'form-control', 'placeholder': 'Теги...'
         }))
+
     class Meta:
         model = Case
-        fields = ['category','region']
+        fields = ['category','region', 'user_id']
 
